967abb3ddae28e79c400e005e0ba62222402a59c
Fony-Stark <kholt19@student.aau.dk>
2020-09-18 10:00:34 +0000

    Peter Help

--- main.c
+++ main.c
@@ -303,8 +303,6 @@ void kill_half_generation(struct NN *gen, int generation_size, int kill){
         } else {
             rand_fact = rand() % (int)ceil(size_gen * 0.1);
         }
-        free(gen[(int)rand_fact].input_layer);
-        free(gen[(int)rand_fact].hidden_layer);
         gen[(int)rand_fact] = empty;
 
         fix_arr(gen, (int)rand_fact, size_gen - i - 1);
